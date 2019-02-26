02a33f7345766b1ea24c3b8489514e79a8beb9a5
NikiArt <genertyniki@gmail.com>
2019-02-26 23:07:22 +0300

    Реализовал получение и отправку СМС

--- app/src/main/java/ru/test/smsmessenger/MainActivity.kt
+++ app/src/main/java/ru/test/smsmessenger/MainActivity.kt
@@ -1,12 +1,56 @@
 package ru.test.smsmessenger
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.telephony.SmsManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var sendButton: Button
+    lateinit var cancelButton: Button
+    lateinit var telNumber: EditText
+    lateinit var textSms: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sendButton = findViewById(R.id.activity_main_button_send)
+        cancelButton = findViewById(R.id.activity_main_button_cancel)
+        telNumber = findViewById(R.id.activity_main_tel_number)
+        textSms = findViewById(R.id.activity_main_sms_text)
+
+        sendButton.setOnClickListener { sendSms() }
+        cancelButton.setOnClickListener { cancelSms() }
+    }
+
+    private fun cancelSms() {
+        if (!textSms.text.isEmpty()) textSms.text.clear()
+    }
+
+    private fun sendSms() {
+        if (!textSms.text.isEmpty()) {
+            /*if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.SEND_SMS)) {
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.SEND_SMS),
+                        MY_PERMISSIONS_REQUEST_SEND_SMS)
+                }
+            }*/
+            SmsManager.getDefault()
+                .sendTextMessage(telNumber.text.toString(), null, textSms.text.toString(), null, null);
+        } else {
+            Toast.makeText(applicationContext, "Введите текст сообщения!", Toast.LENGTH_LONG).show()
+        }
     }
 }
---
+++ app/src/main/java/ru/test/smsmessenger/SmsMonitor.kt
@@ -0,0 +1,25 @@
+package ru.test.smsmessenger
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.NotificationCompat.getExtras
+import android.telephony.SmsMessage
+import android.widget.Toast
+
+
+class SmsMonitor : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent?.action != null && ACTION.compareTo(intent?.action) == 0) {
+            val pduArray = intent?.extras.get("pdus") as Array<Any>
+            val messages = arrayOfNulls<SmsMessage>(pduArray.size)
+            for (i in pduArray.indices) {
+                Toast.makeText(context.applicationContext,(SmsMessage.createFromPdu(pduArray[i] as ByteArray)).messageBody, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    companion object {
+        private val ACTION = "android.provider.Telephony.SMS_RECEIVED"
+    }
+}
